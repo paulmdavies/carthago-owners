e52376cb6cef30d13a2356638b2f9f715a20b57f
Paul Davies <paulmdavies@gmail.com>
2013-12-21 22:17:41 +0000

    Moved the majority of generic page functionality to Page class - working mostly, but slightly broken

--- src/main/scala/com/carthagoowners/IndexPage.scala
+++ src/main/scala/com/carthagoowners/IndexPage.scala
@@ -5,49 +5,15 @@ import scalatags._
 
 class IndexPage extends Page( "index" )
 {
-	def generate() : scala.xml.Node =
+	def body() : HtmlTag =
 	{
-	    html(
-	        head(
-	        	meta.name( "description" ).attr( "content" -> "Carthago Owners: The website of the UK's Carthago owners" ),
-	        	link.attr(
-	        		"rel" -> "stylesheet",
-	        		"type" -> "text/css",
-	        		"media" -> "screen",
-	        		"href" -> "stylesheets.css"
-	        	),
-	        	title( "Carthago Owners" )
-	        ),
-	    	body(
-    	        // header
-    	        div.id( "header_wrap" ).cls( "outer" )
-    	        (
-	                header.cls( "inner" )(
-	                	h1.id( "project_title" )( "Carthago Owners" ),
-	                	h2.id( "project_tagline" )( "The website of the UK's Carthago owners" )
-            		)
-	        	),
-	        	// main body
-	        	div.id( "main_content_wrap" ).cls( "outer" )
-	        	(
-	        		section.id( "main_content" ).cls( "inner" )
-	        		(
-	        			h1(
-	        				a.name( "about-us" ).cls( "anchor" ).href( "#about-us" ),
-	        				span.cls( "octicon octicon-link" )
-	        			)( "About Us" ),
-	        			p( "About the members club or whatever" )
-	        		)
-	        	),
-	        	// footer
-	        	div.id( "footer_wrap" ).cls( "outer" )
-	        	(
-	        		footer.cls( "inner" )
-	        		(
-	        			p.cls( "copyright" )( "Carthago Owners maintained by Paul Davies and Gill Davies" )
-	        		)
-	        	)
-	    	)
-	    ).toXML
+	    bodyContents(
+	        h1(
+				a.name( "about-us" ).cls( "anchor" ).href( "#about-us" )(
+			        span.cls( "octicon octicon-link" )
+		        )
+			)( "About Us" ),
+			p( "About the members club or whatever" )
+		)
 	}
 }
--- src/main/scala/com/carthagoowners/Page.scala
+++ src/main/scala/com/carthagoowners/Page.scala
@@ -1,10 +1,26 @@
 package com.carthagoowners
 
 import java.io.File
+import scalatags._
 
 abstract class Page( val name : String )
 {
-    def generate( ) : scala.xml.Node
+    def body() : HtmlTag
+    
+    def generate( ) : scala.xml.Node =
+    {
+        html(
+	        headContents,
+	    	bodyContents(
+    	        // header
+    	        bodyHeader,
+	        	// main body
+    	        body,
+	        	// footer
+	        	bodyFooter
+	    	)
+	    ).toXML
+    }
     
 	def toFile( parent : File = new File( "." ) ) : Unit =
 	{
@@ -12,4 +28,46 @@ abstract class Page( val name : String )
 	    val node = generate()
 	    scala.xml.XML.save( path, node )
 	}
+    
+    def headContents() : HtmlTag =
+    {
+        head(
+	        meta.name( "description" ).attr( "content" -> "Carthago Owners: The website of the UK's Carthago owners" ),
+			link.attr(
+				"rel" -> "stylesheet",
+				"type" -> "text/css",
+				"media" -> "screen",
+				"href" -> "stylesheets.css"
+			),
+			title( "Carthago Owners" )
+		)
+    }
+    
+    def bodyHeader() : HtmlTag =
+    {
+        div.id( "header_wrap" ).attr( "class" -> "outer" )(
+            header.attr( "class" -> "inner" )(
+            	h1.id( "project_title" )( "Carthago Owners" ),
+            	h2.id( "project_tagline" )( "The website of the UK's Carthago owners" )
+    		)
+    	)
+    }
+    
+    def bodyFooter() : HtmlTag =
+    {
+        div.id( "footer_wrap" ).attr( "class" -> "outer" )(
+    		footer.attr( "class" -> "inner" )(
+    			p.attr( "class" -> "copyright" )( "Carthago Owners maintained by Paul Davies and Gill Davies" )
+    		)
+    	)
+    }
+    
+    def bodyContents( tags : HtmlTag* ) : HtmlTag =
+    {
+        div.id( "main_content_wrap" ).attr( "class" -> "outer" )(
+    		section.id( "main_content" ).attr( "class" -> "inner" )(
+    			tags
+    		)
+    	)
+    }
 }
