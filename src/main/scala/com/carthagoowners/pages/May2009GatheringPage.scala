f0c83d69ee17509d5663ff51a6266b278c7e28f2
Gill Davies <naseby@blueyonder.co.uk>
2014-02-19 19:19:24 +0000

    GatheringMay2009 added

---
+++ src/main/scala/com/carthagoowners/pages/May2009GatheringPage.scala
@@ -0,0 +1,63 @@
+package com.carthagoowners.pages
+
+import scalatags._
+import com.carthagoowners._
+import scala.xml.Unparsed
+
+class May2009GatheringPage extends Page( "gatheringMay2009" ){
+
+ 	def customContents() : Seq[HtmlTag] =
+	{
+	    Seq( 
+	        h1( "2nd UK Gathering - 2009" ),
+	        h2( "Friday 8th May - Sunday 10th May 2009" ),
+	        h3( "Location" ),
+	        p( "Woodland Waters" ),
+	        img.attr( "src" -> "images/gatheringMay2009/Woodland Waters Logo.gif", "alt" -> "Woodland Waters Logo", "width" ->"380"  ),        
+	        h3( "Summary"),
+	        p( "Yet another fabulous weekend with a Gathering of 40 Carthagos and 80+ lovely people." ),
+	        h3 ("Report"),
+	        h6( "by George Pennells" ),
+	        p("Many had arrived on the Thursday and enjoyed a walk around rural Lincolnshire guided by Dave & Chris Davies on the Friday. The walk ended up at The Ermine Way pub in Ancaster just in time for lunch!."),
+	        p("It was a great opportunity for a good old chin wag.  Carthago were represented by Michael Sterk and Paul Mock who both brought their families with them and it was good to see them all.  Lowdhams was represented by Ian Brown. Michael, Paul and Ian were kept very busy with members questions. Meanwhile, other members were comparing their modifications, enhancements and travel tales."),
+	        img.attr( "src" -> "images/gatheringMay2009/Walk 1.jpg", "alt" -> "Walk 1 Photo", "width" ->"380"  ),
+	        img.attr( "src" -> "images/gatheringMay2009/Walk 2.jpg", "alt" -> "Walk 2 Photo", "width" ->"380"  ),
+	        p("Those that were missing arrived during the day and by tea time there were Carthagos of every shape and size spread throughout the rally field and camp site. "),
+	        img.attr( "src" -> "images/gatheringMay2009/Vans 1.jpg", "alt" -> "Vans 1 Photo", "width" ->"380"  ),
+	        img.attr( "src" -> "images/gatheringMay2009/Vans 2.jpg", "alt" -> "Vans 2 Photo", "width" ->"380"  ),
+	        img.attr( "src" -> "images/gatheringMay2009/Vans 3.jpg", "alt" -> "Vans 3 Photo", "width" ->"380"  ),
+	        img.attr( "src" -> "images/gatheringMay2009/Vans 4.jpg", "alt" -> "Vans 4 Photo", "width" ->"380"  ),
+	        p("Everyone assembled in the bar for a barbeque evening which was laid on by the management of Woodland Waters. Unfortunately, the meal did not come up to our expectations but irrespective of that, a good time was had by all. It was a pleasure to make new friends and to welcome old faces."),
+	        p("Saturday morning and many of us boarded a coach for a trip into Lincoln city.  After exploring this lovely old city with its amazing Cathedral and Castle we met up again for a buffet style lunch in The Big Wok. Great Chinese food and you could go up as many times as you wished.  With full tummies we headed for the bus station where we joined the coach for a journey back to the campsite briefly stopping to view Cranwell RAF College.  Throughout the coach journeys Dave Davies gave us a running commentary about the points of interest of his home area."),
+	        p("In the evening we got together again in the bar for a quiz evening. There was some intense competition to win the first place prize of a bottle of wine each. The runners up also received a bottle of wine between two. The wine for the quiz prizes was kindly donated by Lowdhams. The Woodland Waters management also offered everyone a complimentary drink. Many new friends were made and promises of meeting up again at our next Gathering."),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 1.jpg", "alt" -> "Quiz 1 Photo", "width" ->"380" , "height" ->"220"),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 2.jpg", "alt" -> "Quiz 2 Photo", "width" ->"380" , "height" ->"220"),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 3.jpg", "alt" -> "Quiz 3 Photo", "width" ->"380" ),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 4.jpg", "alt" -> "Quiz 4 Photo", "width" ->"380" ),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 5.jpg", "alt" -> "Quiz 5 Photo", "width" ->"380" ),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 6.jpg", "alt" -> "Quiz 6 Photo", "width" ->"380" ),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 7.jpg", "alt" -> "Quiz 7 Photo", "width" ->"380" ),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 8.jpg", "alt" -> "Quiz 8 Photo", "width" ->"380" ),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 9.jpg", "alt" -> "Quiz 9 Photo", "width" ->"380" ),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 10.jpg", "alt" -> "Quiz 10 Photo", "width" ->"380" ),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 11.jpg", "alt" -> "Quiz 11 Photo", "width" ->"380" ),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 12.jpg", "alt" -> "Quiz 12 Photo", "width" ->"380" ),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 13.jpg", "alt" -> "Quiz 13 Photo", "width" ->"380", "height" ->"220" ),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 14.jpg", "alt" -> "Quiz 14 Photo", "width" ->"380" ),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 15.jpg", "alt" -> "Quiz 15 Photo", "width" ->"380" ),
+	        img.attr( "src" -> "images/gatheringMay2009/Quiz 16.jpg", "alt" -> "Quiz 16 Photo", "width" ->"380" ),
+	        p("Sunday morning was bright and sunny and we all met up on the rally field for a coffee and cake session before we headed off to our next destinations."),
+	        img.attr( "src" -> "images/gatheringMay2009/Sunday 1.jpg", "alt" -> "Sunday 1 Photo"  ),
+	        img.attr( "src" -> "images/gatheringMay2009/Sunday 2.jpg", "alt" -> "Sunday 2 Photo"),
+	        img.attr( "src" -> "images/gatheringMay2009/Sunday 3.jpg", "alt" -> "Sunday 3 Photo"  ),
+	        p("So many people told us what an enjoyable weekend it had been and were looking forward to the next Gathering. "),
+	        p("Dave and Christine Davies did a fantastic job of organising the event on our behalf and everything  went off really well. A BIG THANK YOU to both of you. "),
+	        p("Thank you too, to everyone that came along - without you there to support us it just would not happen at all. "),
+	        p("Finally, thank you to Woodland Waters management who had made an outstanding effort to have the function room back in operation for us after a disastrous fire virtually destroyed their building just a few weeks before we were due to arrive."),
+	        p("Chance for a chat").attr("class"->"imagelabel"),
+	        img.attr( "src" -> "images/gatheringMay2009/Chat 1.jpg", "alt" -> "Chat 1 Photo"),
+	        img.attr( "src" -> "images/gatheringMay2009/Chat 2.jpg", "alt" -> "Chat 2 Photo", "width" ->"380" )
+
+        )
+	}
+}
