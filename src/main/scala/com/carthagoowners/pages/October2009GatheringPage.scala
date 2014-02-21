90459f9a3a90c5226cda0bb97da0eb1892df8bab
Gill Davies <naseby@blueyonder.co.uk>
2014-02-21 18:35:02 +0000

    Gathering October 2009 added

---
+++ src/main/scala/com/carthagoowners/pages/October2009GatheringPage.scala
@@ -0,0 +1,56 @@
+package com.carthagoowners.pages
+
+import scalatags._
+import com.carthagoowners._
+
+class October2009GatheringPage extends Page( "gatheringOctober2009" )
+{
+	def customContents() : Seq[HtmlTag] =
+	{
+	    Seq( 
+	        h1( "European Gathering - 2009" ),
+	        h2( "Thursday 1st October - Tuesday 6th October 2009" ),
+	        h3( "Location" ),
+	        p( "Hemau - Regensburg" ),
+	        h3( "Summary"),
+	        p( "This year we joined Carthago Kreis (the German Owners Club) in Hemau, nr. Regensburg, Bavaria. There were 18 Carthagos from the UK and approx. 84 Carthagos from Germany and other European countries." ),	        
+	        h3 ("Report"),
+	        h6( "by George Pennells" ),
+	        p( "We were overwhelmed by the warm welcome we received from Carthago Kreis. They made sure that we took part in all the events and everyone made an attempt to speak English to us. Most of them had an exceptionally good command of our language." ),
+	        p( "Thursday evening we all gathered in the Hall for a meal which was provided by the Carthago Kreis members. Beer and wine was available and all drinks were 1 euro. It was a fantastic evening. We met so many lovely people and were entertained by 'The Carthagos' band. There was plenty of dancing and singing." ),
+	        img.attr( "src" -> "images/gatheringOctober2009/Carthagos 1.jpg", "alt" -> "Carthagos 1" ),	        
+	        img.attr( "src" -> "images/gatheringOctober2009/Carthagos 2.jpg", "alt" -> "Carthagos 2" ),	        
+	        p( "Friday morning was an early start as the coach left at 08:45 for a tour of Regensburg. We met our guide who gave as a very informative tour and spoke very good English. Regensburg is a beautiful old city and made it's fortune in the middle ages by being the only safe place to cross the Danube via the first stone bridge in Germany. Apparently, other cities along the Danube, such as Vienna, also had bridges but these were wooden and were prone to be washed away. Consequently, these bridges were taken down before the flood season to preserve them and so Regensburg became the only bridge point at certain times of the year." ),
+	        img.attr( "src" -> "images/gatheringOctober2009/Regensburg Tour.png", "alt" -> "Regensburg Tour" ),	        
+	        img.attr( "src" -> "images/gatheringOctober2009/Regensburg.png", "alt" -> "Regensburg" ),	        
+	        img.attr( "src" -> "images/gatheringOctober2009/Bus Stop.png", "alt" -> "Waiting for the bus" ),	        
+	        p( "After the formal tour we split up to explore the city on our own and later caught the public bus back to Hemau. We had been told that if we travelled as a group of 5 we could get a much reduced bus fare. The result of this was that each  bus was absolutely packed with Carthago Owners getting cheap fares. The locals could not believe their eyes when their bus, normally with a few people on board, arrived at the bus stop full to overflowing! " ),
+	        p( "Friday evening back at Hemau was a quiet evening and time was spent getting to know new friends." ),
+	        p( "Saturday morning and Carthago Owners UK gathered around in the sunshine for an informal meeting. Again George asked for contributions for the forthcoming newsletters.  The timing of the Spring and Autumn meetings was discussed and it was agreed that it would be preferable to move both meetings by 2 weeks each, to bring them nearer the warmer weather and longer evenings. The revised dates for 2010 are now on this website." ),
+	        img.attr( "src" -> "images/gatheringOctober2009/Saturday.png", "alt" -> "Saturday night" ),
+	        p( "Early Saturday evening was the main get together.  We were treated to a magnificent buffet and were entertained again by 'The Carthagos'. We knew a few of the dances by now but still had not quite got to grips with the Lasso dance. The Brits were about half a second behind the Germans in getting the appropriate actions right!" ),
+	        p( "We were also entertained by the local mayor who gave a lengthy speech in German which went completely over our heads. Apparently, he was only going to pop in for an hour or so but ended up staying all evening. The British ladies made sure that they got him on to the dance floor for a few energetic dances - well done ladies!" ),
+	        p( "The star event of the evening was a group of lederhosen clad men who performed a variety of Bavarian traditional routines. One particular routine involved cracking horse whips in time to the music. To do this they stood on benches between the dinners. I am not sure which was more exciting for our ladies, the whips or the thighs in the leather shorts!" ),
+	        img.attr( "src" -> "images/gatheringOctober2009/Sarah.png", "alt" -> "Sarah" ),	  
+	        img.attr( "src" -> "images/gatheringOctober2009/Rolf.jpg", "alt" -> "Rolf" ),	  
+	   	    img.attr( "src" -> "images/gatheringOctober2009/Martin.jpg", "alt" -> "Martin" ),	       	        
+	        img.attr( "src" -> "images/gatheringOctober2009/Performers 1.jpg", "alt" -> "Performers 1" ),	        
+	        img.attr( "src" -> "images/gatheringOctober2009/Performers 2.png", "alt" -> "Performers 2" ),	        
+	        img.attr( "src" -> "images/gatheringOctober2009/Performers 3.jpg", "alt" -> "Performers 3" ),	        
+	        p( "Sunday morning and we were invited to join in a farewell breakfast which comprised two white Bavarian sausages and a bread roll.   After breakfast many German vans departed.  There were no formal arrangements for Sunday and everyone did their own thing." ),
+	        p( "Monday we took the local bus into Regensburg and took a river boat tour along the Danube. After the trip we were treated to a sample of sausages cooked in the very old inn by the stone bridge.  Allegedly, this little inn had served food to the workers who built the bridge in the 12th century." ),
+	        p( "After another walk around the city we took the bus back to Hemau (group ticket of course!) as Ray & Julie had arranged a barbeque for us in the evening. Another excellent meal despite the fact that we were all huddled under the awning and garage door to avoid the rain!  Thanks Ray and Julie for a good evening!" ),
+	        img.attr( "src" -> "images/gatheringOctober2009/Boat 1.jpg", "alt" -> "Boat 1", "width" ->"480" ),	        
+	        img.attr( "src" -> "images/gatheringOctober2009/Boat 2.jpg", "alt" -> "Boat 2", "width" ->"480" ),	        
+	        img.attr( "src" -> "images/gatheringOctober2009/Boat 3.png", "alt" -> "Boat 3", "width" ->"480" ),	        
+	        img.attr( "src" -> "images/gatheringOctober2009/Boat 4.png", "alt" -> "Boat 4", "width" ->"480" ),	        
+	        img.attr( "src" -> "images/gatheringOctober2009/Boat 5.jpg", "alt" -> "Boat 5", "width" ->"480" ),	        
+	        p( "Tuesday was another early start. Today it was a 7:30am departure for the BMW factory tour. The coach that took us to the factory also served as our transport within the factory as we visited different areas of the 346 acre site." ),
+	        p( "It was truly amazing to watch the robots assembling the cars. 1,200 cars per day are being produced at this one site. We saw the body panels being pressed and later assembled and painted before moving on to the assembly line to meet up with the engines and gearboxes and have the final items added. We were intrigued by the unmanned transporters that moved components around the assembly floor.  This was a really interesting tour and although it took approximately 2 hours 45minutes we all felt as if we had been rushed around and could have spent longer just watching the amazing antics of the robots. Unfortunately, no photography was allowed." ),
+	        img.attr( "src" -> "images/gatheringOctober2009/BMW.png", "alt" -> "BMW" ),	        
+	        p( "We returned to Hemau and after a meal many of the UK members departed for the next adventure.  Those that remained departed on Wednesday." ),
+	        p( "Overall it was a fantastic weekend. We shall never forget the very warm welcome that we received and the most enjoyable entertainment. Many of us made new, Carthago owner friends and Hemau will be remembered as a wonderful event.  Our sincere thanks to Rolf and Marita Jungbluth and to all the other members of Carthago Kreis who worked so hard to make this a special weekend for us." ),
+	        p( "" )
+	)
+	}
+}
